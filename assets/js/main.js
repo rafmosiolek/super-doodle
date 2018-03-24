18c015a7673cd47555784178590275d150d66558
RafM <rkjmosiolek@gmail.com>
2018-03-24 17:06:44 +0000

    set up the gulp

---
+++ assets/js/main.js
@@ -0,0 +1,22 @@
+// const locationResult = document.querySelector("#location-result h1");
+// const getLocationCta = document.querySelector("#location-cta");
+
+
+
+
+// getLocationCta.addEventListener('click', () => {
+//     console.log('wuwu');
+//     if (navigator.geolocation) {
+//         navigator.geolocation.getCurrentPosition(successCallback, errorCallback, {timeout:10000});
+//     }
+// });
+
+// const successCallback = (position) => {
+//     let lat = position.coords.latitude;
+//     let lng = position.coords.longtitude;
+//     locationResult.innerHTML = "Your location is: " + lat + "&deg;, " + lng + "&deg;.";
+// }
+
+// const errorCallback = (error) => {
+//     locationResult.innerHTML = "Geolocation is not supported by this browser!" + error.code;
+// }
