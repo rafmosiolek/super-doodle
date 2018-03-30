74bd1ad359ce5f92d6f600e0a859c17b34db9190
RafM <rkjmosiolek@gmail.com>
2018-03-30 23:42:21 +0100

    add functionality and UI

--- assets/js/main.js
+++ assets/js/main.js
@@ -5,69 +5,91 @@ const initMap = () => {
             lat: 51.5074,
             lng: -0.1278
         },
-        zoom: 13
+        zoom: 13,
+        mapTypeId: 'terrain'
     }
     const map = new google.maps.Map(mapContainer, mapOptions);
     const directionsService = new google.maps.DirectionsService;
     const directionsDisplay = new google.maps.DirectionsRenderer;
     directionsDisplay.setMap(map);
-    
-    let destination = new google.maps.Marker({
-        position: {
-            lat: 51.5074,
-            lng: -0.1278
-        },
-        map: map,
-        animation: google.maps.Animation.DROP,
-    });
-    
-    let userPosition = new google.maps.Marker({
-        position: {
-            lat: 51.490264,
-            lng:  -0.143563
-            },
-        map: map,
-        animation: google.maps.Animation.DROP,
-    });
 
-   
-    let start = destination.position;
-    let end = userPosition.position;
+    let userLocation;
+    if (navigator.geolocation) { 
+        console.log('geolocation');
+        let options = {
+            enableHighAccuracy: true,
+            timeout: 5000,
+            maximumAge: 0
+        }
+        const errorCallback = (browserGeolocation, pos) => {
+            browserGeolocation ? console.log('Error: The Geolocation service failed.') : console.log('Error: Your browser doesn\'t support geolocation.');
+        }
+        console.log(options);
+        navigator.geolocation.getCurrentPosition((position, options) => {
+            console.log('found');
+            
+            userLocation = {
+                lat: position.coords.latitude,
+                lng: position.coords.longitude
+            }
+            let userPosition = new google.maps.Marker({
+                position: userLocation,
+                map: map,
+                animation: google.maps.Animation.DROP,
+            });
+            console.log(userLocation);
+            map.setCenter(userLocation);
+            
+        }, () => {
+            errorCallback(true, map.getCenter());
+        });
+    } else {
+        errorCallback(false, map.getCenter());
+    }
+    
+    
+    
+    // let destination = new google.maps.Marker({
+    //     position: {
+    //         lat: 51.5074,
+    //         lng: -0.1278
+    //     },
+    //     map: map,
+    //     animation: google.maps.Animation.DROP,
+    // });
+    
+    // let userPosition = new google.maps.Marker({
+    //     position: {
+    //         lat: 51.490264,
+    //         lng:  -0.143563
+    //         },
+    //     map: map,
+    //     animation: google.maps.Animation.DROP,
+    // });
 
-    calculateAndDisplayRoute(directionsService, directionsDisplay, start, end);
-    // console.log(directionsService, directionsDisplay, start, end);
-    // if (navigator.geolocation) { 
-    //     console.log('geolocaction')
-    //     navigator.geolocation.getCurrentPosition((position) => {
-    //         console.log('found');
-    //         let userLocation = {
-    //             lat: position.coords.latitude,
-    //             lng: position.coords.longitude
-    //         }
-    //         let userPositionMarker = new google.maps.Marker({
-    //             position: {
-    //                 lat: userLocation.lat,
-    //                 lng: userLocation.lng
-    //             },
-    //             map: map,
-    //             animation: google.maps.Animation.DROP,
-    //         })
-    //         console.log(userLocation);
-    //         map.setCenter(userLocation);
-    //     }, () => {
-    //         errorCallback(true, map.getCenter());
-    //     });
-    // } else {
-    //     errorCallback(false, map.getCenter());
-    // }
-    // const errorCallback = (browserGeolocation, pos) => {
-    //     browserGeolocation ? console.log('Error: The Geolocation service failed.') : console.log('Error: Your browser doesn\'t support geolocation.');
-    // }
+
+
+    // const locationBtn = document.querySelector("#find-user");
+    // const routeBtn = document.querySelector("#find-route");
+    const routeBtn = document.querySelector("#find-route");    
+    
+    routeBtn.addEventListener('click', () => {
+        console.log('click route btn');
+        calculateAndDisplayRoute(directionsService, directionsDisplay, userLocation);
+    });
 }
 
-const calculateAndDisplayRoute = (directionsService, directionsDisplay, startingPoint, destination) => {
+
+
+
+
+const calculateAndDisplayRoute = (directionsService, directionsDisplay, startingPoint) => {
     console.log('calculateRoute called');
     
+    let destination = {
+        lat: 51.5074,
+        lng: -0.1278
+    }
     directionsService.route(
         {
             origin: startingPoint,
--- build/js/main.js
+++ build/js/main.js
@@ -1,4 +1,4 @@
-"use strict";
+'use strict';
 
 var initMap = function initMap() {
     var mapContainer = document.querySelector("#map");
@@ -7,68 +7,83 @@ var initMap = function initMap() {
             lat: 51.5074,
             lng: -0.1278
         },
-        zoom: 13
+        zoom: 13,
+        mapTypeId: 'terrain'
     };
     var map = new google.maps.Map(mapContainer, mapOptions);
     var directionsService = new google.maps.DirectionsService();
     var directionsDisplay = new google.maps.DirectionsRenderer();
     directionsDisplay.setMap(map);
 
-    var destination = new google.maps.Marker({
-        position: {
-            lat: 51.5074,
-            lng: -0.1278
-        },
-        map: map,
-        animation: google.maps.Animation.DROP
+    var userLocation = void 0;
+    if (navigator.geolocation) {
+        console.log('geolocation');
+        var options = {
+            enableHighAccuracy: true,
+            timeout: 5000,
+            maximumAge: 0
+        };
+        var _errorCallback = function _errorCallback(browserGeolocation, pos) {
+            browserGeolocation ? console.log('Error: The Geolocation service failed.') : console.log('Error: Your browser doesn\'t support geolocation.');
+        };
+        console.log(options);
+        navigator.geolocation.getCurrentPosition(function (position, options) {
+            console.log('found');
+
+            userLocation = {
+                lat: position.coords.latitude,
+                lng: position.coords.longitude
+            };
+            var userPosition = new google.maps.Marker({
+                position: userLocation,
+                map: map,
+                animation: google.maps.Animation.DROP
+            });
+            console.log(userLocation);
+            map.setCenter(userLocation);
+        }, function () {
+            _errorCallback(true, map.getCenter());
+        });
+    } else {
+        errorCallback(false, map.getCenter());
+    }
+
+    // let destination = new google.maps.Marker({
+    //     position: {
+    //         lat: 51.5074,
+    //         lng: -0.1278
+    //     },
+    //     map: map,
+    //     animation: google.maps.Animation.DROP,
+    // });
+
+    // let userPosition = new google.maps.Marker({
+    //     position: {
+    //         lat: 51.490264,
+    //         lng:  -0.143563
+    //         },
+    //     map: map,
+    //     animation: google.maps.Animation.DROP,
+    // });
+
+
+    // const locationBtn = document.querySelector("#find-user");
+    // const routeBtn = document.querySelector("#find-route");
+    var routeBtn = document.querySelector("#find-route");
+
+    routeBtn.addEventListener('click', function () {
+        console.log('click route btn');
+        calculateAndDisplayRoute(directionsService, directionsDisplay, userLocation);
     });
-
-    var userPosition = new google.maps.Marker({
-        position: {
-            lat: 51.490264,
-            lng: -0.143563
-        },
-        map: map,
-        animation: google.maps.Animation.DROP
-    });
-
-    var start = destination.position;
-    var end = userPosition.position;
-
-    calculateAndDisplayRoute(directionsService, directionsDisplay, start, end);
-    // console.log(directionsService, directionsDisplay, start, end);
-    // if (navigator.geolocation) { 
-    //     console.log('geolocaction')
-    //     navigator.geolocation.getCurrentPosition((position) => {
-    //         console.log('found');
-    //         let userLocation = {
-    //             lat: position.coords.latitude,
-    //             lng: position.coords.longitude
-    //         }
-    //         let userPositionMarker = new google.maps.Marker({
-    //             position: {
-    //                 lat: userLocation.lat,
-    //                 lng: userLocation.lng
-    //             },
-    //             map: map,
-    //             animation: google.maps.Animation.DROP,
-    //         })
-    //         console.log(userLocation);
-    //         map.setCenter(userLocation);
-    //     }, () => {
-    //         errorCallback(true, map.getCenter());
-    //     });
-    // } else {
-    //     errorCallback(false, map.getCenter());
-    // }
-    // const errorCallback = (browserGeolocation, pos) => {
-    //     browserGeolocation ? console.log('Error: The Geolocation service failed.') : console.log('Error: Your browser doesn\'t support geolocation.');
-    // }
 };
 
-var calculateAndDisplayRoute = function calculateAndDisplayRoute(directionsService, directionsDisplay, startingPoint, destination) {
+var calculateAndDisplayRoute = function calculateAndDisplayRoute(directionsService, directionsDisplay, startingPoint) {
     console.log('calculateRoute called');
 
+    var destination = {
+        lat: 51.5074,
+        lng: -0.1278
+    };
     directionsService.route({
         origin: startingPoint,
         destination: destination,
